997ad303abad46af9e03be8911ef7b4c76c9946e
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-01 00:25:49 +0800

    Create config/config.php Make sitename configurable

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Laravel</a>
+            <a class="navbar-brand" href="#">{{ Config::get('config.sitename') }}</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
