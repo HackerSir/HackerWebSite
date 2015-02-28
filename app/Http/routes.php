b870ec341e558f2a2cf84f620351123cd64dda68
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-02-28 23:56:36 +0800

    Remove auth and password in route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,8 +15,3 @@ Route::get('/', [
     'as' => 'home',
     'uses' => 'HomeController@index'
 ]);
-
-Route::controllers([
-    'auth' => 'Auth\AuthController',
-    'password' => 'Auth\PasswordController',
-]);
