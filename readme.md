abf552d10a4984212a992c900aa6ed8726a1f1ce
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-02-28 23:37:44 +0800

    Copy basic website from MCV

---
+++ readme.md
@@ -0,0 +1,31 @@
+# HackerWebSite
+黑客社網站
+
+## 網址
+unset
+
+## 基礎架構
+- PHP
+- MySQL
+
+## 主要框架
+- Laravel 5.0
+- Bootstrap 3
+
+## 文件格式
+- 編碼：UTF8
+- 縮排：4 spaces
+- 換行：LF
+- 去除行尾空格
+- 文件結尾有1空白行
+
+（已使用EditorConfig設定完成）
+
+## 推薦IDE
+- Sublime Text
+- PhpStorm
+
+（專案使用[EditorConfig](http://editorconfig.org/)定義基本格式，請安裝與IDE對應的套件）
+
+## 推薦Chrome擴充套件
+- [Bitbucket Notifier](https://chrome.google.com/webstore/detail/bitbucket-notifier/jncnafmlekinjbdlcncbpclfpmgodcmf)
---
+++ resources/views/common/navbar.blade.php
@@ -0,0 +1,56 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Laravel</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                {{-- 左側主要選單 --}}
+                @foreach (Config::get('navbar.navbar') as $name => $uri)
+                    @if(is_array($uri))
+                        {{-- 次級選單 --}}
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ $name }}<span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                @foreach ($uri as $subName => $subUri)
+                                    {{-- 一般項目 --}}
+                                    <li @if((strstr(Request::route()->getPath(),$subUri) && $subUri!="/") || Request::route()->getPath()==$subUri) class="active" @endif>
+                                    <a href="{{ URL::to($subUri) }}">{{ $subName }}</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @else
+                        {{-- 一般項目 --}}
+                        <li @if((strstr(Request::route()->getPath(),$uri) && $uri!="/") || Request::route()->getPath()==$uri) class="active" @endif>
+                            <a href="{{ URL::to($uri) }}">{{ $name }}</a>
+                        </li>
+                    @endif
+                @endforeach
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                {{-- 右側選單 --}}
+                {{-- TODO:以下未修改 --}}
+                @if (Auth::guest())
+                    <li><a href="./auth/login">Login</a></li>
+                    <li><a href="./auth/register">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="./auth/logout">Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
