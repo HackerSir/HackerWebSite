b724c9ba069fc901317466b6415d6704474a6528
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-01 17:10:13 +0800

    Use HTML generator instead of script tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,16 +6,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ Config::get('config.sitename') }}</title>
 
-        <link href="{{URL::route('home')}}/css/app.css" rel="stylesheet">
+        {!! HTML::style('css/app.css'); !!}
 
         <!-- Fonts -->
-        <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+        {!! HTML::style('//fonts.googleapis.com/css?family=Roboto:400,300'); !!}
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+            {!! HTML::script('https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'); !!}
+            {!! HTML::script('https://oss.maxcdn.com/respond/1.4.2/respond.min.js'); !!}
         <![endif]-->
     </head>
     <body>
@@ -34,7 +34,7 @@
         </div>
 
         <!-- Scripts -->
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+        {!! HTML::script('//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'); !!}
+        {!! HTML::script('//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js'); !!}
     </body>
 </html>
