95733cca74ee304ca70be5705b7ba091867125dc
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-02 21:37:25 +0800

    add 418 page

---
+++ resources/views/errors/418.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <div class="row text-center">
+            {!! HTML::image('pic/418.jpg','I'm a teapot'); !!}
+        </div>
+    </div>
+@endsection
