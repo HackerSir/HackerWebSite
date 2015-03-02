6a95068a5c2de9c3d3b313a73a8f165a466e0e2a
Chan, Danny <danny50610@yahoo.com.tw>
2015-03-02 22:48:32 +0800

    Add 4**, 5** image

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section('title')
+    401
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row text-center">
+            {!! HTML::image('pic/401.jpg','Unauthorized'); !!}
+        </div>
+    </div>
+@endsection
