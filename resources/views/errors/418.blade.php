98f416817f4b7d2fb47c61802a8b2b01181c12f0
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-02 21:38:03 +0800

    fix 418 page

--- resources/views/errors/418.blade.php
+++ resources/views/errors/418.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row text-center">
-            {!! HTML::image('pic/418.jpg','I'm a teapot'); !!}
+            {!! HTML::image('pic/418.jpg','I\'m a teapot'); !!}
         </div>
     </div>
 @endsection
