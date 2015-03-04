ef98e116a1463c104cf6f57f9a581f82b2f9f2cb
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-04 21:32:09 +0800

    Fix redirect target of middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -34,7 +34,7 @@ class RedirectIfAuthenticated {
     {
         if ($this->auth->check())
         {
-            return new RedirectResponse(url('/home'));
+            return new RedirectResponse(route('home'));
         }
 
         return $next($request);
