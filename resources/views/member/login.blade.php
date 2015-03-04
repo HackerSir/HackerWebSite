10d0fb78645dcb49b3864ff418b91714488dc8eb
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-04 20:24:30 +0800

    Update login / register page

--- resources/views/member/login.blade.php
+++ resources/views/member/login.blade.php
@@ -21,23 +21,23 @@
                         <div role="tabpanel" class="tab-pane active" id="login">
                             {{-- 登入 --}}
                             {!! Form::open(['route' => 'member.login']) !!}
-                                <div class="form-group has-feedback {{ ($errors->has('email'))?'has-error':'' }}">
-                                    <label for="email">信箱
+                                <div class="form-group has-feedback{{ ($errors->has('email'))?' has-error':'' }}">
+                                    <label class="control-label" for="email">信箱
                                         @if($errors->has('email'))
                                             <span class="label label-danger">{{ $errors->first('email') }}</span>
-                                            <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                                         @endif
                                     </label>
                                     {!! Form::email('email', null, ['placeholder' => '請輸入信箱', 'class' => 'form-control', 'required']) !!}
+                                    @if($errors->has('email'))<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>@endif
                                 </div>
-                                <div class="form-group has-feedback {{ ($errors->has('password'))?'has-error':'' }}">
-                                    <label for="password">密碼 <a href="{{ URL::route('member.forgot-password') }}" tabindex="4">（忘記密碼）</a>
+                                <div class="form-group has-feedback{{ ($errors->has('password'))?' has-error':'' }}">
+                                    <label class="control-label" for="password">密碼 <a href="{{ URL::route('member.forgot-password') }}" tabindex="4">（忘記密碼）</a>
                                         @if($errors->has('password'))
                                             <span class="label label-danger">{{ $errors->first('password') }}</span>
-                                            <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                                         @endif
                                     </label>
                                     {!! Form::password('password', ['placeholder' => '請輸入密碼', 'class' => 'form-control', 'required']) !!}
+                                    @if($errors->has('password'))<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>@endif
                                 </div>
                                 <div class="form-group">
                                     {!! Form::checkbox('remember', 'remember') !!}
@@ -49,32 +49,32 @@
                         <div role="tabpanel" class="tab-pane" id="register">
                             {{-- 註冊 --}}
                             {!! Form::open(['route' => 'member.register']) !!}
-                            <div class="form-group has-feedback {{ ($errors->has('email'))?'has-error':'' }}">
-                                <label for="email">信箱
+                            <div class="form-group has-feedback{{ ($errors->has('email'))?' has-error':'' }}">
+                                <label class="control-label" for="email">信箱
                                     @if($errors->has('email'))
                                         <span class="label label-danger">{{ $errors->first('email') }}</span>
-                                        <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                                     @endif
                                 </label>
                                 {!! Form::email('email', null, ['placeholder' => '請輸入信箱', 'class' => 'form-control', 'required']) !!}
+                                @if($errors->has('email'))<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>@endif
                             </div>
-                            <div class="form-group has-feedback {{ ($errors->has('password'))?'has-error':'' }}">
-                                <label for="password">密碼
+                            <div class="form-group has-feedback{{ ($errors->has('password'))?' has-error':'' }}">
+                                <label class="control-label" for="password">密碼
                                     @if($errors->has('password'))
                                         <span class="label label-danger">{{ $errors->first('password') }}</span>
-                                        <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                                     @endif
                                 </label>
                                 {!! Form::password('password', ['placeholder' => '請輸入密碼', 'class' => 'form-control', 'required']) !!}
+                                @if($errors->has('password'))<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>@endif
                             </div>
-                            <div class="form-group has-feedback {{ ($errors->has('password_again'))?'has-error':'' }}">
-                                <label for="password_again">密碼（再輸入一次）
+                            <div class="form-group has-feedback{{ ($errors->has('password_again'))?' has-error':'' }}">
+                                <label class="control-label" for="password_again">密碼（再輸入一次）
                                     @if($errors->has('password_again'))
                                         <span class="label label-danger">{{ $errors->first('password_again') }}</span>
-                                        <span class="glyphicon glyphicon-remove form-control-feedback"></span>
                                     @endif
                                 </label>
                                 {!! Form::password('password_again', ['placeholder' => '請再輸入一次密碼', 'class' => 'form-control', 'required']) !!}
+                                @if($errors->has('password_again'))<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>@endif
                             </div>
                             {!! Form::submit('註冊', ['class' => 'btn btn-primary']) !!}
                             {!! Form::close() !!}
