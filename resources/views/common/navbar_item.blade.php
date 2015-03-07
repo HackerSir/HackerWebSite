374b82be05e5aec7b3270bec2eeb4a7cf5c46a11
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-07 21:46:20 +0800

    Fix navbar

--- resources/views/common/navbar_item.blade.php
+++ resources/views/common/navbar_item.blade.php
@@ -2,12 +2,12 @@
     @if(is_array($uri))
         {{-- 次級選單 --}}
         <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ str_replace('%user%',Auth::user()->name,$name) }}<span class="caret"></span></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ (Auth::check())?str_replace('%user%',Auth::user()->name,$name):$name }}<span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
                 @foreach ($uri as $subName => $subUri)
                     {{-- 一般項目 --}}
                     <li @if((strstr(Request::route()->getPath(),$subUri) && $subUri!="/") || Request::route()->getPath()==$subUri) class="active" @endif>
-                        <a href="{{ URL::to($subUri) }}">{{ str_replace('%user%',Auth::user()->name,$subName) }}</a>
+                        <a href="{{ URL::to($subUri) }}">{{ (Auth::check())?str_replace('%user%',Auth::user()->name,$subName):$subName }}</a>
                     </li>
                 @endforeach
             </ul>
@@ -15,7 +15,7 @@
     @else
         {{-- 一般項目 --}}
         <li @if((strstr(Request::route()->getPath(),$uri) && $uri!="/") || Request::route()->getPath()==$uri) class="active" @endif>
-            <a href="{{ URL::to($uri) }}">{{ str_replace('%user%',Auth::user()->name,$name) }}</a>
+            <a href="{{ URL::to($uri) }}">{{ (Auth::check())?str_replace('%user%',Auth::user()->name,$name):$name }}</a>
         </li>
     @endif
 @endforeach
