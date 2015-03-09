bb7aff5e767a76934dd860b9efb073cabedbab05
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-09 22:34:56 +0800

    Update home page sections

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,36 @@
 
 @section('content')
 <div id="fullpage">
+    {{-- Welcome section--}}
     <div class="section" style="background-image: url('{{ URL::asset('pic/welcome.jpg') }}'); background-size: 100%;">
 
     </div>
+    {{-- About section--}}
     <div class="section">
-        <h1>關於我們</h1>
-        {!! HTML::image('http://placehold.it/1024x768') !!}
+        <div class="col-md-6">
+            {!! HTML::image('http://placehold.it/1024x768', null, ['width' => '100%']) !!}
+        </div>
+        <div class="col-md-6">
+            <h1>關於我們</h1>
+        </div>
     </div>
+    {{-- Class section--}}
     <div class="section text-right">
-        <h1>社團課程</h1>
-        {!! HTML::image('http://placehold.it/1024x768') !!}
+        <div class="col-md-6">
+            <h1>社團課程</h1>
+        </div>
+        <div class="col-md-6">
+            {!! HTML::image('http://placehold.it/1024x768', null, ['width' => '100%']) !!}
+        </div>
     </div>
+    {{-- Activity section--}}
     <div class="section">
-        <h1>社團活動</h1>
-        {!! HTML::image('http://placehold.it/1024x768') !!}
+        <div class="col-md-6">
+            {!! HTML::image('http://placehold.it/1024x768', null, ['width' => '100%']) !!}
+        </div>
+        <div class="col-md-6">
+            <h1>社團活動</h1>
+        </div>
     </div>
 </div>
 @endsection
