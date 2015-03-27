43cdee70214108fe9bffc4a2d698ce75379f96f2
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-27 21:00:50 +0800

    Open external links of navbar in new window  (check if url contains protocol type)

--- resources/views/common/navbar_item.blade.php
+++ resources/views/common/navbar_item.blade.php
@@ -1,3 +1,8 @@
 <li @if((Request::is($uri) && $uri!="/") || Request::route()->getPath()==$uri) class="active" @endif>
-    <a href="{{ URL::to($uri) }}">{{ (Auth::check())?str_replace('%user%',Auth::user()->nickname,$name):$name }}</a>
+    <a href="{{ URL::to($uri) }}" @if(strpos($uri,'://')) target="_blank" @endif>
+        {{ (Auth::check())?str_replace('%user%',Auth::user()->nickname,$name):$name }}
+        @if(strpos($uri,'://'))
+            <i class="glyphicon glyphicon-new-window"></i>
+        @endif
+    </a>
 </li>
