4974d92911fef5b7727ceb3719272aaeac9f6a2d
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-27 21:09:15 +0800

    Fix external links logic

--- resources/views/common/navbar_item.blade.php
+++ resources/views/common/navbar_item.blade.php
@@ -1,7 +1,7 @@
 <li @if((Request::is($uri) && $uri!="/") || Request::route()->getPath()==$uri) class="active" @endif>
-    <a href="{{ URL::to($uri) }}" @if(strpos($uri,'://')) target="_blank" @endif>
+    <a href="{{ URL::to($uri) }}" @if(strpos($uri,'://') !== false) target="_blank" @endif>
         {{ (Auth::check())?str_replace('%user%',Auth::user()->nickname,$name):$name }}
-        @if(strpos($uri,'://'))
+        @if(strpos($uri,'://') !== false)
             <i class="glyphicon glyphicon-new-window"></i>
         @endif
     </a>
