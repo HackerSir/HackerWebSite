6cef9ba64ee15add4578b966885797f2984c9e1a
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-03-28 18:29:19 +0800

    Add group of SA

---
+++ database/migrations/2015_03_28_182820_add_groups_data.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGroupsData extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Group::create(array('name' => 'sa', 'title' => '學生會'));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Group::where('name', '=', 'sa')->delete();
+    }
+
+}
