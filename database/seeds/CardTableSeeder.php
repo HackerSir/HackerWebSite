75978a833a0d1d5e2fcc79bb9df2d3321a6c7b4d
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-04-04 19:37:38 +0800

    Update CardTableSeeder

--- database/seeds/CardTableSeeder.php
+++ database/seeds/CardTableSeeder.php
@@ -29,5 +29,24 @@ class CardTableSeeder extends Seeder
 
             $this->command->info("[$i] Add card: " . $card->nid . " - " . $card->card_number);
         }
+        //建立未綁定卡片
+        for ($i = 0; $i < 50; $i++) {
+            //班級
+            $department = array("資訊", "資電", "電機", "外文", "國貿", "會計", "統計");
+            $year = array("一", "二", "三", "四");
+            $class = array("甲", "乙", "丙");
+            shuffle($department);
+            shuffle($year);
+            shuffle($class);
+            $grade = $department[0] . $year[0] . $class[0];
+
+            $card = Card::create(array(
+                'nid' => $faker->regexify('[depm]([0-9]){7})'),
+                'grade' => $grade,
+                'name' => $faker->name
+            ));
+
+            $this->command->info("[$i] Add card: " . $card->nid . " - " . $faker->name);
+        }
     }
 }
