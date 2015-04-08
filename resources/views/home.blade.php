f2d972013a777be1fb2688474e2e898a0796c433
jyhsu2000 <jyhsu2000@yahoo.com.tw>
2015-04-08 22:24:20 +0800

    Update home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,34 +3,28 @@
 @section('content')
 <div id="fullpage">
     {{-- Welcome section--}}
-    <div class="section" id="section0">
+    <div class="section" id="section1">
 
     </div>
     {{-- About section--}}
-    <div class="section">
-        <div class="col-md-6">
-            {!! HTML::image('http://placehold.it/1024x768', null, ['width' => '100%']) !!}
-        </div>
-        <div class="col-md-6">
+    <div class="section" id="section2">
+        <div class="col-md-6 col-md-offset-6 section-text">
             <h1>關於我們</h1>
+
         </div>
     </div>
     {{-- Class section--}}
-    <div class="section text-right">
-        <div class="col-md-6">
+    <div class="section text-right" id="section3">
+        <div class="col-md-6 section-text">
             <h1>社團課程</h1>
-        </div>
-        <div class="col-md-6">
-            {!! HTML::image('http://placehold.it/1024x768', null, ['width' => '100%']) !!}
+
         </div>
     </div>
     {{-- Activity section--}}
-    <div class="section">
-        <div class="col-md-6">
-            {!! HTML::image('http://placehold.it/1024x768', null, ['width' => '100%']) !!}
-        </div>
-        <div class="col-md-6">
+    <div class="section" id="section4">
+        <div class="col-md-6 col-md-offset-6 section-text">
             <h1>社團活動</h1>
+
         </div>
     </div>
 </div>
@@ -41,11 +35,29 @@
         padding-top: 0px;
     }
 
-    #section0 {
-        background-image: url({{ URL::asset('pic/welcome.jpg') }});
+    .section {
         background-size: 100%;
         background-repeat: no-repeat;
         background-position: center center;
+    }
+
+    .section-text {
+        background: rgba(255,255,255,0.5);
+        height: 100%;
+        color: black;
+    }
+
+    #section1 {
+        background-image: url({{ URL::asset('pic/welcome.jpg') }});
+    }
+    #section2 {
+        background-image: url(http://placehold.it/1024x768);
+    }
+    #section3 {
+        background-image: url(http://placehold.it/1024x768);
+    }
+    #section4 {
+        background-image: url(http://placehold.it/1024x768);
     }
 @endsection
 
