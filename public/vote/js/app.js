32af9079eace6bd0b3f49b7d6b28c250e383c66c
root <root@hackersir.info>
2015-04-09 09:08:38 -0400

    Revert "Revert "Update app.js""
    This reverts commit e34d529eae36b8f25dae1c8a7337aaa911b0a8c6.
    
    Conflicts:
    	public/vote/js/app.js

--- public/vote/js/app.js
+++ public/vote/js/app.js
@@ -1,5 +1,5 @@
-var ref$, map, zip, API_URL, graphOptions, template, charts, makeChart, updateCharts, fetchBooth, fetchVotes, currentBooth, intervalHandle, setBooth;
-ref$ = require('prelude-ls'), map = ref$.map, zip = ref$.zip;
+var ref$, map, max, API_URL, graphOptions, template, datas, charts, makeChart, updateCharts, fetchBooth, fetchVotes, intervalHandle, setBooth;
+ref$ = require('prelude-ls'), map = ref$.map, max = ref$.max;
 API_URL = '../vote-api';
 graphOptions = function(title){
   return {
@@ -24,6 +24,7 @@ template = function(data){
     }]
   };
 };
+datas = [];
 charts = [];
 makeChart = function(data){
   var x$, canvas;
@@ -34,7 +35,12 @@ makeChart = function(data){
   return canvas.getContext('2d');
 };
 updateCharts = function(data){
+<<<<<<< HEAD
   for (i$ = 0, to$ = max(charts, data.votes.length) - 1; i$ < to$; ++i$) {
+=======
+  var i$, to$, i, ctx, votesData, results$ = [];
+  for (i$ = 0, to$ = max(charts, data.votes) - 1; i$ < to$; ++i$) {
+>>>>>>> parent of e34d529... Revert "Update app.js"
     i = i$;
     ctx = charts[i];
     votesData = data.votes[i];
@@ -48,21 +54,21 @@ fetchBooth = function(cb){
 fetchVotes = function(id, cb){
   return $.get(API_URL + "/votes" + id, cb);
 };
-currentBooth = "/0";
 intervalHandle = null;
 setBooth = function(path){
   return fetchVotes(path, function(votesData){
     var res$, i$, ref$, len$, d;
     $('#booth-name').text(votesData.name);
-    $('#embed-video').attr('src', votesData.url);
     if (path === '') {
       $('#canvases').removeClass('canvases');
       $('#video-column').addClass('hide');
       $('#votes-column').removeClass('col-md-4');
+      $('#embed-video').attr('src', null);
     } else {
       $('#canvases').addClass('canvases');
       $('#video-column').removeClass('hide');
       $('#votes-column').addClass('col-md-4');
+      $('#embed-video').attr('src', votesData.url);
     }
     if (charts.length === 0) {
       res$ = [];
@@ -77,10 +83,9 @@ setBooth = function(path){
     if (intervalHandle !== null) {
       clearInterval(intervalHandle);
     }
-    intervalHandle = setInterval(function(){
+    return intervalHandle = setInterval(function(){
       return fetchVotes(path, updateCharts);
     }, 30000);
-    return console.log(intervalHandle);
   });
 };
 window.onload = function(){
