7f4a56b62283af82469e35d56b349ec7b7eca767
Chan, Danny <danny50610@yahoo.com.tw>
2015-04-25 16:11:01 +0800

    Add table caption

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,6 +47,7 @@
                     每堂社課皆是 07:00 PM 至 09:00 PM！
                 </p>
                 <table class="tg table table-striped">
+                    <caption style="color: #000000">課程名稱：<span class="label label-success">請問您今天要來點Python嗎？</span></caption>
                     <thead>
                     <tr class="active">
                         <th>日期</th>
@@ -60,85 +61,76 @@
                     <tr class="success">
                         <td>3/10 (二)</td>
                         <td>語文401</td>
-                        <td class="tg-left">請問您今天要來點Python嗎？Day 1<br />
-                            &gt;&gt;&gt; 安裝、基礎操作</td>
+                        <td class="tg-left">Day 1<br />&gt;&gt;&gt;安裝、基礎操作</td>
                         <td>Jyny</td>
                         <td>Finished</td>
                     </tr>
                     <tr class="success">
                         <td>3/14 (二)</td>
                         <td>商學301</td>
-                        <td class="tg-left">請問您今天要來點Python嗎？Day 2<br />
-                            &gt;&gt;&gt; 字串</td>
+                        <td class="tg-left">Day 2<br />&gt;&gt;&gt;字串</td>
                         <td>Tim</td>
                         <td>Finished</td>
                     </tr>
                     <tr class="success">
                         <td>3/24 (二)</td>
                         <td>商學301</td>
-                        <td class="tg-left">請問您今天要來點Python嗎？Day 3<br />
-                            &gt;&gt;&gt; 格式化、日期與時間</td>
+                        <td class="tg-left">Day 3<br />&gt;&gt;&gt;格式化、日期與時間</td>
                         <td>Danny</td>
                         <td>Finished</td>
                     </tr>
                     <tr class="success">
                         <td>3/31 (二)</td>
                         <td>商學301</td>
-                        <td class="tg-left">請問您今天要來點Python嗎？Day 4<br />
-                            &gt;&gt;&gt; 函式、遞迴</td>
+                        <td class="tg-left">Day 4<br />&gt;&gt;&gt;函式、遞迴</td>
                         <td>Vongola</td>
                         <td>Finished</td>
                     </tr>
                     <tr class="warning">
                         <td>4/28 (二)</td>
                         <td>資電B19</td>
-                        <td class="tg-left">請問您今天要來點Python嗎？Day 5<br />
-                            &gt;&gt;&gt; 遞迴、迴圈</td>
+                        <td class="tg-left">Day 5<br />&gt;&gt;&gt;遞迴、迴圈</td>
                         <td>Vongola</td>
                         <td>Next</td>
                     </tr>
                     <tr class="info">
                         <td>5/05 (二)</td>
                         <td>資電B19</td>
-                        <td class="tg-left">請問您今天要來點Python嗎？Day 6<br />
-                            &gt;&gt;&gt; List、Dictionary</td>
+                        <td class="tg-left">Day 6<br />&gt;&gt;&gt;List、Dictionary</td>
                         <td>?</td>
                         <td></td>
                     </tr>
                     <tr class="info">
                         <td>5/12 (二)</td>
                         <td>資電B19</td>
-                        <td class="tg-left">請問您今天要來點Python嗎？Day 7<br />
-                            &gt;&gt;&gt; List + 迴圈應用</td>
+                        <td class="tg-left">Day 7<br />&gt;&gt;&gt;List + 迴圈應用</td>
                         <td>?</td>
                         <td></td>
                     </tr>
                     <tr class="info">
                         <td>5/26 (二)</td>
                         <td>資電B19</td>
-                        <td class="tg-left">請問您今天要來點Python嗎？Day 8<br />
-                            &gt;&gt;&gt; Review</td>
+                        <td class="tg-left">Day 8<br />&gt;&gt;&gt;Review</td>
                         <td>?</td>
                         <td></td>
                     </tr>
                     <tr class="info">
                         <td>6/02 (二)</td>
                         <td>資電B19</td>
-                        <td class="tg-left">請問您今天要來點Python嗎？Day 9 <br />
-                            &gt;&gt;&gt; Class</td>
+                        <td class="tg-left">Day 9 <br />&gt;&gt;&gt;Class</td>
                         <td>?</td>
                         <td></td>
                     </tr>
                     <tr class="info">
                         <td>6/09 (二)</td>
                         <td>資電B19</td>
-                        <td class="tg-left">請問您今天要來點Python嗎？Day 10<br />
-                            &gt;&gt;&gt; File I/O<br /></td>
+                        <td class="tg-left">Day 10<br />&gt;&gt;&gt;File I/O</td>
                         <td>?</td>
                         <td></td>
                     </tr>
                     </tbody>
                 </table>
+                <a href="/course" class="btn btn-primary" role="button">課程清單&nbsp;<span class="glyphicon glyphicon-th-list" aria-hidden="true"></span></a>
             </div>
         </div>
         {{-- Activity section--}}
