067bef21f3239db1949d71f1e0f9fda61c571b7f
Chan, Danny <danny50610@yahoo.com.tw>
2015-04-28 22:57:53 +0800

    Fixed indent

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta property="og:image" content="http://hackersir.info/pic/Header.jpg">
+	    <meta property="og:image" content="http://hackersir.info/pic/Header.jpg">
         <title>@if (trim($__env->yieldContent('title'))) @yield('title') - @endif{{ Config::get('config.sitename') }}</title>
 
         {!! HTML::style('css/app.css'); !!}
