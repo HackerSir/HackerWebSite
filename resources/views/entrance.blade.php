a0d99bb69f2770f7c452cc6476f1386c5ebc0745
Vongola <vongola12324@coder.tw>
2015-06-04 14:38:38 +0800

    set image alpha

--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -8,14 +8,14 @@
     body{
         background-color: #2D2535;
     }
-    .centered{
-        margin: 0 auto;
+    .img-alpha{
+        filter: Alpha(Opacity=30, FinishOpacity=30, Style=2);
     }
 @endsection
 
 @section('content')
     <div class="center-block">
-        {!! HTML::image('pic/entrance.png', '', array('class'=>'img-responsive')) !!}
+        {!! HTML::image('pic/entrance.png', '', array('class'=>'img-responsive img-alpha')) !!}
     </div>
 
 @endsection
