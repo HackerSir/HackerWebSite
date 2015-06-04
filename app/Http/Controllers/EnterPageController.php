caeefd11269e5221f237fddb85dd6bae67ca7a37
Vongola <vongola12324@coder.tw>
2015-06-04 18:48:20 +0800

    fix bug ._.

--- app/Http/Controllers/EnterPageController.php
+++ app/Http/Controllers/EnterPageController.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Session;
 
-class HomeController extends Controller
+class EnterPageController extends Controller
 {
 
     /*
