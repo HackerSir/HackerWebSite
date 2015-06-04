ffcb8cde28b2f4173fb2a48491f073d0dd477660
Vongola <vongola12324@coder.tw>
2015-06-04 14:32:36 +0800

    remove flush

--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -11,7 +11,7 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid">
+    <div class="container">
         <div class="centered">
             {!! HTML::image('pic/entrance.png', '', array('class'=>'img-responsive')) !!}
         </div>
