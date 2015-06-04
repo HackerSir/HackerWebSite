a8e859c027ddb1f7016aea842e4a31dee80d212c
Vongola <vongola12324@coder.tw>
2015-06-04 15:33:45 +0800

    Finish entrance

--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -11,12 +11,8 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-6 col-sm-4">
-                {!! HTML::image('pic/entrance.png', '', array('class'=>'img-responsive')) !!}
-            </div>
-        </div>
+    <div class="text-center">
+        {!! HTML::image('pic/entrance.png', '', array('class'=>'img-responsive center-block')) !!}
+        <a class="btn btn-primary btn-lg" href="{!! URL::route('home') !!}" role="button">進入社網</a>
     </div>
-
 @endsection
