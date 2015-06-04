1b17d6cddda1178331f13a896d6d865277af7a7d
Vongola <vongola12324@coder.tw>
2015-06-04 14:31:33 +0800

    let image center - use different class

--- resources/views/entrance.blade.php
+++ resources/views/entrance.blade.php
@@ -11,8 +11,8 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-6 col-lg-offset-3 text-center">
+    <div class="container-fluid">
+        <div class="centered">
             {!! HTML::image('pic/entrance.png', '', array('class'=>'img-responsive')) !!}
         </div>
     </div>
